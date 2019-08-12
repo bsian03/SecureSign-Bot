d87b3cbb04a7baaf4e6f314801ed9f2bf9bde6dd
Bsian <bsian@staff.libraryofcode.us>
2019-08-12 20:03:15 +0100

    Update init.js

--- commands/init.js
+++ commands/init.js
@@ -15,7 +15,6 @@ module.exports = () => {
         try {
             const userConfig = require('../userCache');
             // eslint-disable-next-line prefer-const
-            if (userConfig.blacklists.includes(msg.author.id)) return;
             if (msg.channel.type !== 1) {
                 if (args[0]) msg.delete();
                 msg.channel.createMessage('Run this command in your DMs!');
